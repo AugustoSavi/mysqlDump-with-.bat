e18370380b32110050422363af7403da0b95d59f
Augusto Savi <32443720+AugustoSavi@users.noreply.github.com>
2020-07-20 17:37:46 -0300

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -5,4 +5,4 @@ set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
 set "fullstamp=%YYYY%%MM%%DD%_%HH%%Min%%Sec%"
 
-mysqldump -p bruning_bonfante -h localhost -u root -pbru!@# --port 3311 > C:\BruningSistemas\retaguarda\bkp\bruning_bonfante_%fullstamp%.sql
+mysqldump -p [database] -h [host] -u root -p[password] --port [port] > [directory]\[database]_%fullstamp%.sql
